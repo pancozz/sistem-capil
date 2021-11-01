a83210a504c5210877c963696e5fad69efda632a
Ahmad Affandy <pancozgnoxz@gmail.com>
2021-11-01 13:00:36 +0800

    User & Admin Page

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -44,7 +44,11 @@ class Auth extends CI_Controller
                         'role_id' => $user['role_id']
                     ];
                     $this->session->set_userdata($data);
-                    redirect('user');
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else {
+                        redirect('user');
+                    }
                 } else {
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
                     Password salah</div>');
