87fbd5f7cc8488e38b5bd78a587cf6d1fc8306b0
Ahmad Affandy <pancozgnoxz@gmail.com>
2021-11-09 10:42:20 +0800

    Fitur Ganti Password

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -72,4 +72,56 @@ class User extends CI_Controller
             redirect('user');
         }
     }
+
+    public function changepassword()
+    {
+        $data['title'] = 'Ganti Password';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('current_password', 'Current Password', 'required|trim', [
+            'required' => 'Harap mengisi Password Sebelumnya'
+        ]);
+        $this->form_validation->set_rules('new_password1', 'Password', 'required|trim|min_length[3]|matches[new_password2]', [
+            'required' => 'Harap mengisi Password Baru',
+            'matches' => 'Password tidak sama',
+            'min_length' => 'Password minimal 3 karakter'
+        ]);
+        $this->form_validation->set_rules('new_password2', 'Password', 'required|trim|matches[new_password1]', [
+            'required' => 'Harap mengisi Ulangi Password',
+            'matches' => 'Password tidak sama',
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/changepassword', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $current_password = $this->input->post('current_password');
+            $new_password = $this->input->post('new_password1');
+            if (!password_verify($current_password, $data['user']['password'])) {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Password saat ini salah!</div>');
+                redirect('user/changepassword');
+            } else {
+                if ($current_password == $new_password) {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Password baru tidak boleh sama dengan sebelumnya!</div>');
+                    redirect('user/changepassword');
+                } else {
+                    $password_hash = password_hash($new_password, PASSWORD_DEFAULT);
+
+                    $this->db->set('password', $password_hash);
+                    $this->db->where()('email', $this->session->userdata('email'));
+                    $this->db->update('user');
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                    Password berhasil diganti!</div>');
+                    redirect('user/changepassword');
+                }
+            }
+        }
+    }
 }
--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -25,9 +25,13 @@
                     </a>
                     <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">
+                        <a class="dropdown-item" href="<?= base_url('user'); ?>">
                             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                             Profil
+                        </a>
+                        <a class="dropdown-item" href="<?= base_url('user/changepassword'); ?>">
+                            <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Ganti Password
                         </a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
