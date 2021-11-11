dd4b8c645fa22fc6ddc57dea9242f76459d14002
Ahmad Affandy <pancozgnoxz@gmail.com>
2021-11-11 15:35:50 +0800

    Fix Error

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('user'); ?>">
         <div class="sidebar-brand-icon">
             <img src="<?= base_url('assets/img/logo/img.png') ?>" class="mx-auto d-block mb-1" width="auto" height="60px">
         </div>
